81f481387d7b9344274af9dd2d9dd058d236d1e4
Daniel <danielodonovan20@gmail.com>
2025-09-14 14:02:52 +0100

    Initial commit - Repo setup

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("service-worker.js")
+        .then(() => console.log("Service Worker registered"))
+        .catch(err => console.error("Service Worker failed:", err));
+    }
+
+
+
